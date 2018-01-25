bfaa110f60e9fab764f4ed29081456a337ce0194
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-01-25 15:00:15 -0600

    done

--- learning-materials.js
+++ learning-materials.js
@@ -1,15 +1,25 @@
-const CardMaker = {
+
+  //Refactor this to link this object to cardMaker
+  
+  const CardMaker = {
     makeFrontMessage: function(string) {
       return `<h2>${string}</h2>`
     },
-
+    makeInsideMessage: function(string) {
+      return `<h2>${string}</h2>`
+    }
+    
   }
   
   //Refactor this to link this object to cardMaker
   const BirthdayCard = Object.create(CardMaker);
+  const NewYearsCard = Object.create(CardMaker);
+  const XmasCard = Object.create(CardMaker);
 
-  BirthdayCard.makeFrontMessage("Happy Birthday!");
+
+  console.log(BirthdayCard.makeFrontMessage("birthday front"), BirthdayCard.makeInsideMessage("birthday inside"));
+  console.log(NewYearsCard.makeFrontMessage("new years front"), NewYearsCard.makeInsideMessage("new years inside"));
+  console.log(XmasCard.makeFrontMessage("xmas front"), XmasCard.makeInsideMessage("xmas inside"));
 
 
-  console.log(BirthdayCard.makeFrontMessage);
   
